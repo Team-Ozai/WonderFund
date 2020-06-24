9f8dfdb970e6244bde0800fcf361940aaf20979a
Orlando <jcsf1991@gmail.com>
2020-06-23 17:19:56 -0700

    update mysql login information

--- server/model/database.js
+++ server/model/database.js
@@ -5,8 +5,8 @@ const createConnection = ()=>{
   return new Promise ((resolve, reject) =>{
     mysql
       .createConnection({
-        user     : 'root',
-        password : ''
+        user     : DATABASE_USER,
+        password : DATABASE_PASSWORD
       })
       .then((connection) => {
         connection.query('CREATE DATABASE IF NOT EXISTS campaign')
